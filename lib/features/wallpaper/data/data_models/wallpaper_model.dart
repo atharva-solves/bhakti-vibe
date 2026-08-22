07336147e207f4efc7f208e30c4d57eb2e0da60e
Atharva Shinde <atharva.solves@gmail.com>
2026-08-22 15:25:14 +0530

    refactor:added fromtrendingJson factory Constructor to make Aarti Model also from Trending Json response because it has different key category_image not images

--- lib/features/wallpaper/data/data_models/wallpaper_model.dart
+++ lib/features/wallpaper/data/data_models/wallpaper_model.dart
@@ -3,7 +3,11 @@ import 'package:bhakti_vibe/features/wallpaper/domain/entities/wallpaper_entity.
 class WallpaperModel extends WallpaperEntity {
   WallpaperModel({required super.id, required super.images});
 
-  factory WallpaperModel.fromJson({required Map<String, dynamic> json}) {
+  factory WallpaperModel.fromGodCategoryJson({required Map<String, dynamic> json}) {
     return WallpaperModel(id: json['id'], images: json['images'] ?? '');
+  }
+
+  factory WallpaperModel.fromTrendingJson({required Map<String, dynamic> json}) {
+    return WallpaperModel(id: json['id'], images: json['post_image'] ?? '');
   }
 }
